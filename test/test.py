02d00c6a9b8c28da7819e94991032aa5dc3bc9b4
Amna Aftab <amnaaftab94@gmail.com>
2015-11-28 16:23:28 -0500

    randomized data

--- test/test.py
+++ test/test.py
@@ -1,9 +1,15 @@
 import re
 import sys 
 import pickle  
-import nltk
-from nltk.tokenize import word_tokenize
+import nltk, nltk.classify.util, nltk.metrics
+import collections
 import string 
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.util import ngrams
+from random import shuffle
+
 # don't actually need this anymore
 from textblob.classifiers import NaiveBayesClassifier
 
@@ -23,7 +29,12 @@ def readFile(path):
 	return text.split('\n')
 
 def cleanData(line):
-	return ' '.join(word.strip(string.punctuation) for word in line.split()) 
+	no_stop = []
+	for word in line.split():
+		if word not in stopwords.words('english'):
+			no_stop.append(word.lower())
+	remove_punct = ' '.join(word.strip(string.punctuation) for word in no_stop) 
+	return remove_punct
 
 # take in a some array of strings (each index has a sentence)
 # return the first 3/4 of the array of strings for training data
@@ -49,12 +60,10 @@ def tag_sentence(text, feature):
 	for t in text:
 		tup = () 
 		t = t.replace("'", "")
-		t = ' '.join(word.strip(string.punctuation) for word in t.split()) 
+		t = cleanData(t) # ' '.join(word.strip(string.punctuation) for word in t.split()) 
 		tup = (t, feature)
-		#data[t] = feature
 		data.append(tup)
 	return data
-
 # combine pos neg data
 def load_data(file, tag, training_or_testing):
 	txt = readFile(file)
@@ -62,7 +71,6 @@ def load_data(file, tag, training_or_testing):
 		txt = getTraining(txt)
 	if training_or_testing is "testing":
 		txt = getTesting(txt)
-	#txt = getTraining(txt)
 	txt = tag_sentence(txt, tag) 
 	return txt
 
@@ -78,7 +86,8 @@ def train(training_data):
 def extract_features(phrase):
 	words = nltk.word_tokenize(phrase)
 	features = {}
-	for word in words: 
+	new_features = {}
+	for word in words:
 		features['contains(%s)' % word] = (word in words)
 	return features
 
@@ -97,25 +106,71 @@ def load_classifier(filename = 'classifier.dump'):
 	ifile.close()
 	return classifier
 
-def getData(training_or_testing):
-	data = load_data(negativeData1, "neg", training_or_testing)
-	data += load_data(positiveData, "pos", training_or_testing)
-	data += load_data(negativeData2, "neg", training_or_testing)
-	data += load_data(negativeData3, "neg", training_or_testing)
-	return data
+# takes in some text, shuffles, splits into pos and neg
+def getShuffledData(txt, tag):
+	shuffle(txt)
+	length = len(txt)
+	numLines = float(length*3)/4
+	training_data = txt[:int(numLines)]
+	testing_data = txt[int(numLines):]
+	training_data = tag_sentence(training_data, tag)
+	testing_data = tag_sentence(testing_data, tag)
+	return (training_data, testing_data)
 
-# load data, parse and tag
-training_data = getData("training")
-len(training_data)
-testing_data = getData("testing")
-len(testing_data)
+def getData():
+	pos_data = readFile(positiveData)
+	neg_data_1 = readFile(negativeData1)
+	neg_data_2 = readFile(negativeData2)
+	neg_data_3 = readFile(negativeData3)
+	neg_data = neg_data_1 + neg_data_2 + neg_data_3
+	return (pos_data, neg_data)
 
-# train and save data 
-# data = train(trainingData)
-# save(data)
+def save_to_file(text, filename):
+	f = open(filename, 'w')
+	f.write('\n'.join(str(v) for v in text))
+	f.close()
+
+
+all_data = getData()
+pos_data = getShuffledData(all_data[0], "pos")
+neg_data = getShuffledData(all_data[1], "neg")
+
+shuffled_training_data = pos_data[0] + neg_data[0] 
+shuffled_testing_data = pos_data[1] + neg_data[1]
+
+save_to_file(shuffled_training_data, 'training_data.txt')
+save_to_file(shuffled_testing_data, 'testing_data.txt')
+
+# train and save data - training data takes a while 
+#data = train(shuffled_testing_data)
+#save(data)
 classifier = load_classifier()
-print test_classifier(classifier, testing_data)
+
+errors = [] 
+for (prompt, tag) in shuffled_testing_data:
+	guess = classifier.classify(extract_features(prompt))
+	if guess != tag:
+		errors.append((tag, guess, prompt))
+
+for (tag, guess, name) in sorted(errors):
+	print('correct={:<8} guess={:<8s} name={:<30}'.format(tag, guess, name))
+
+# tests classifier, prints out the accuracy 
+print test_classifier(classifier, shuffled_testing_data)
+# prints out the most informative features (which words had the biggest impact)
 print classifier.show_most_informative_features()
+
+
+# refsets = collections.defaultdict(set)
+# testsets = collections.defaultdict(set)
+ 
+# for i, (feats, label) in enumerate(testing_data):
+# 	refsets[label].add(i)
+# 	observed = classifier.classify(extract_features(feats))
+# 	testsets[observed].add(i)
+
+#print extract_features("Ahh, it hit me right in the face.")
+#print cleanData("Ahh, it hit me right in the face.")
 #print loadTestData(negativeData1)
 #print test(data, "Ahh, it hit me right in the face.")
 # # some docs are longer than others, 
@@ -128,4 +183,13 @@ print classifier.show_most_informative_features()
 #f = open('parsedData.txt', 'w')
 #f.write('\n'.join(str(v) for v in trainingData))
 #f.close()
+
+# def getData(training_or_testing):
+# 	data = load_data(negativeData1, "neg", training_or_testing)
+# 	data += load_data(negativeData2, "neg", training_or_testing)
+# 	data += load_data(negativeData3, "neg", training_or_testing)
+# 	print 'negative data: ', len(data), ' ', training_or_testing
+# 	data += load_data(positiveData, "pos", training_or_testing)
+# 	print 'positive data: ', len(data), ' ', training_or_testing
+# 	return data
 
